b5db86831649285bd1adaf4e26883d9333e63d82
Jhon1197 <jhon.alvarez.ttc.771@unilibre.edu.co>
2024-11-13 22:03:10 -0500

    Ejercicio 4

---
+++ script4.js
@@ -0,0 +1,7 @@
+let numero =Math.floor(Math.randon() * 10) + 1;
+let adivina = prompt ("Adivina un numero entre 1 y 10");
+if (adivina==numero){
+    document.write("Felicidades, adivinaste el número");
+}else {
+    document.write("Lo siento no adivinaste")
+}
