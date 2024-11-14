bc692b26e03d068271331ed8ce1840ce0d864d58
Jhon1197 <jhon.alvarez.ttc.771@unilibre.edu.co>
2024-11-13 21:13:49 -0500

    Ejercicio 1

---
+++ script1.js
@@ -0,0 +1,12 @@
+let calificacion=parseInt(prompt("Ingrese tu calificación"));
+if(calificacion>90){
+    console.log("Aprobo con Honores");
+    document.write("Aprobo con Honores");
+}else if (calificacion>=70){
+    console.log("Aprobo")
+  document.write("Aprobo");
+} 
+  else{
+    console.log("reprobo");
+    document.write("reprobo");
+}
