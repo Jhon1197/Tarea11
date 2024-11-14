db4b8f7a52fc21f89202cda52fe84d1180297aab
Jhon1197 <jhon.alvarez.ttc.771@unilibre.edu.co>
2024-11-13 21:45:47 -0500

    Cambios

--- script3.js
+++ script3.js
@@ -2,7 +2,7 @@ let monto =parseInt(prompt("Digite por favor el monto de su compra"));
 if(monto >100 ){
 
     let porcentaje = monto * 0.9;
-    document.write("Su compra tiene un descuento del 10%", porcentaje, "<br>");
+    document.write("Su compra tiene un descuento del 10%", "<br>", porcentaje );
 }else 
-    document.write("Su compra no aplica para el descuento", monto, "<br>");
+    document.write("Su compra no aplica para el descuento","<br>", monto );
 
