55ef265af04b3b0ec4677f67ee749396bfe659f3
Jhon1197 <jhon.alvarez.ttc.771@unilibre.edu.co>
2024-11-13 22:06:00 -0500

    Cambios

--- script4.js
+++ script4.js
@@ -1,7 +1,7 @@
 let numero =Math.floor(Math.randon() * 10) + 1;
-let adivina = prompt ("Adivina un numero entre 1 y 10");
+let adivina = prompt("Adivina un numero entre 1 y 10");
 if (adivina==numero){
     document.write("Felicidades, adivinaste el número");
 }else {
-    document.write("Lo siento no adivinaste")
+    document.write("Lo siento no adivinaste");
 }
